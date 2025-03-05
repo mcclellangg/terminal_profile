e866c976883efea386bb6550abbb1d8e16eeedec
Grayson <mcclellangg@gmail.com>
2025-03-04 20:36:19 -0500

    inital commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,61 @@
+# INSTALL MODULES
+# Install-Module -Name Terminal-Icons
+
+# IMPORTS
+Import-Module -Name Terminal-Icons
+Import-Module -Name PS-Menu
+
+# VARIABLES
+$profile_path = "C:\Users\mccle\Documents\PowerShell"
+$dev = "C:\Users\mccle\dev"
+$para = "C:\Users\mccle\PARA"
+$repos = "C:\Users\mccle\dev\repos"
+
+# FUNCTIONS
+function dev {
+    Set-Location -Path $dev
+    Get-ChildItem 
+}
+
+function para {
+    Set-Location -Path $para
+}
+
+function st {
+    # Set-Title
+    $path = Get-Location
+    $lowest_directory = Split-Path -path $path -Leaf
+    $host.UI.RawUI.WindowTitle = $lowest_directory
+}
+
+function cdx {
+    # Display code repos, and navigate to selected.
+    $repo_map = [ordered]@{} # hash tables don't normally perserve order
+    $sorted_dirs = Get-ChildItem -Path $repos -Directory | Sort-Object LastWriteTime -Descending
+    
+    # Populate hash table
+    $sorted_dirs | ForEach-Object { $repo_map[$_.Name] = $_.FullName }
+    $repo_name = menu @($repo_map.keys)
+    $selected_repo = $repo_map[$repo_name]
+    Set-Location -Path $selected_repo
+    Get-ChildItem # Display repo contents to user
+}
+
+function help() {
+    Write-Output "--------------"
+    Write-Output "NAVIGATION";
+    Write-Output "--------------"
+    Write-Output "dev - navigate to dev, and display directory";
+    Write-Output "para - navigate to PARA directory";
+
+    Write-Output "--------------"
+    Write-Output "UTILITIES";
+    Write-Output "--------------"
+    Write-Output "st - Renames the title window for terminal to current lowest directory";
+    Write-Output "cdx - Displays a menu of code repos, changes location to one selected by user";
+}
+
+# MAIN
+Set-PSReadlineKeyHandler -Chord Alt+F4 -Function ViExit # Add close window shortcut
+Set-Location -Path $dev
+oh-my-posh init pwsh --config "C:\Users\mccle\dev\terminal_themes\dracula.omp.json" | Invoke-Expression
